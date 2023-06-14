ea60ca18fae0e66d4da1ca5c22255f86dae83e13
79096 <V9880S@gmail.com>
2023-06-14 17:54:45 +0300

    check1

--- main.py2.py
+++ main.py2.py
@@ -1,13 +1,13 @@
-def greet():
-    print("-------------------")
-    print("  Приветсвуем вас  ")
-    print("      в игре       ")
-    print("  крестики-нолики  ")
-    print("-------------------")
-    print(" формат ввода: x y ")
-    print(" x - номер строки  ")
-    print(" y - номер столбца ")
-
+ef
+greet():
+print("-------------------")
+print("  Приветсвуем вас  ")
+print("      в игре       ")
+print("  крестики-нолики  ")
+print("-------------------")
+print(" формат ввода: x y ")
+print(" x - номер строки  ")
+print(" y - номер столбца ")
 
 greet()
 
@@ -79,32 +79,5 @@ while True:
         field[x][y] = "0"
 
     if num == 9:
-        "break"
+        break
         print("Ничья")
-
-
-        def check_win():
-            win_cord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
-                        ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
-                        ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
-            for cord in win_cord:
-                symbols = []
-            for c in cord:
-                symbols.append(field[c[0]][c[1]])
-            if symbols == ["X", "X", "X"]:
-                print("Выиграл X!!!")
-                return True
-            if symbols == ["0", "0", "0"]:
-                print("Выиграл 0!!!")
-                return True
-            return False
-
-
-        "check_win()"
-
-
-
-
-
-
-
